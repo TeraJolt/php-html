48e01610e55365569d9e32f299db01caabfc4479
TeraJolt <jollimas14@outlook.com>
2024-03-11 20:33:31 -0300

    merge

---
+++ source/php/formulario.php
@@ -0,0 +1,16 @@
+<?php
+    $nome=$_POST['nome'];
+    $endereco=$_POST['endereco'];
+    $numero=$_POST['numero'];
+    $bairro=$_POST['bairro'];
+    $cidade=$_POST['cidade'];
+    $uf_estado=$_POST['estado'];
+    $cep=$_POST['cep'];
+    $telefone=$_POST['telefone'];
+    $celular=$_POST['celular'];
+    $email=$_POST['email'];
+    $data_nasc=$_POST['data_nasc'];
+    $est_civil=$_POST['est_civ'];
+
+    echo $nome.$endereco.$numero.$bairro.$cidade.$uf_estado.$cep.$telefone.$celular.$email.$data_nasc.$est_civil
+?>
